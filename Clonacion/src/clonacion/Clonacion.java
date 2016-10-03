8798a1f9c7863baf9162dd2c71b65dc7f70c6f0c
csantamariacameselle <csantamariacameselle@atanasoff19.danielcastelao.org>
2016-10-03 09:51:05 +0200

    Inicio

---
+++ Clonacion/src/clonacion/Clonacion.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clonacion;
+
+/**
+ *
+ * @author csantamariacameselle
+ */
+public class Clonacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.err.print("Este es la primera modificacion");
+        
+    }
+    
+}
